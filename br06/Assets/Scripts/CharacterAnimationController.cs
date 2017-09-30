a3d4e1a69dd817397772e65b4739d47200a7a06c
Jonathan Singer <14136988@studentmail.ul.ie>
2017-10-01 00:41:21 +0100

    Second character

--- br06/Assets/Scripts/CharacterAnimationController.cs
+++ br06/Assets/Scripts/CharacterAnimationController.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class CharacterAnimationController : MonoBehaviour {
 
-    static Animator animator;
-    public float walkSpeed = 7.5f;
-    public float runSpeed = 17.5f;
-    public float rotationSpeed = 200.0f;
-    public float strafeSpeed = 5.0f;
+    private Animator animator;
     private bool running;
 
+    // Speed variables
+    public float walkSpeed = 7.5f;
+    public float runSpeed = 17.5f;
+    public float turnSpeed = 200.0f;
+    public float strafeSpeed = 5.0f;
+
+    // Key bindings
+    public KeyCode walkKey = KeyCode.W;
+    public KeyCode walkBackKey = KeyCode.S;
+    public KeyCode turnLeftKey = KeyCode.A;
+    public KeyCode turnRightKey = KeyCode.D;
+    public KeyCode strafeLeftKey = KeyCode.Q;
+    public KeyCode strafeRightKey = KeyCode.E;
+    public KeyCode runKey = KeyCode.LeftShift;
+
     void Start () {
-        animator = GetComponent<Animator>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     void Update() {
-        // Handle input and movement
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetAxis("Vertical") >= 0)
+        /*if (Input.GetKey(runKey) && Input.GetAxis("Vertical") >= 0)
             running = true;
         else
             running = false;
 
         float translation;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        float rotation = Input.GetAxis("Horizontal") * turnSpeed;
         float strafeTranslation = 0;
         if (running)
             translation = Input.GetAxis("Vertical") * runSpeed;
@@ -33,14 +43,41 @@ public class CharacterAnimationController : MonoBehaviour {
         rotation *= Time.deltaTime;
 
         transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
+        transform.Rotate(0, rotation, 0);*/
 
-        if (!Input.GetKey(KeyCode.LeftShift))
-            if (Input.GetKey(KeyCode.Q)) {
+        // Handle input and movement
+        float translation = 0.0f;
+        float rotation = 0.0f;
+        float strafeTranslation = 0.0f;
+
+        if (Input.GetKey(walkKey))
+        {
+            if (Input.GetKey(runKey))
+                translation = runSpeed * Time.deltaTime;
+            else
+                translation = walkSpeed * Time.deltaTime;
+            transform.Translate(0, 0, translation);
+        }
+        else if (Input.GetKey(walkBackKey)) {
+            translation = -walkSpeed * Time.deltaTime;
+            transform.Translate(0, 0, translation);
+        }
+
+        if (Input.GetKey(turnLeftKey)) {
+            rotation = -turnSpeed * Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+        }
+        else if (Input.GetKey(turnRightKey)) {
+            rotation = turnSpeed * Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+        }
+
+        if (!Input.GetKey(runKey))
+            if (Input.GetKey(strafeLeftKey)) {
                 strafeTranslation = -strafeSpeed * Time.deltaTime;
                 transform.Translate(strafeTranslation, 0, 0);
             }
-            else if (Input.GetKey(KeyCode.E)) {
+            else if (Input.GetKey(strafeRightKey)) {
                 strafeTranslation = strafeSpeed * Time.deltaTime;
                 transform.Translate(strafeTranslation, 0, 0);
             }
@@ -48,7 +85,7 @@ public class CharacterAnimationController : MonoBehaviour {
         // Handle animations
         if (translation > 0) {
             animator.SetInteger("walking", 1);
-            if (running)
+            if (Input.GetKey(runKey))
                 animator.SetBool("running", true);
             else
                 animator.SetBool("running", false);
