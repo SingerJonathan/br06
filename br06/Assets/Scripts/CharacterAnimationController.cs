8fdf416e0de9c49e8069e8dcc7e10fa087d66383
Jonathan Singer <14136988@studentmail.ul.ie>
2018-01-09 13:21:11 +0000

    #26: Further simplified movement model by removing walking

--- br06/Assets/Scripts/CharacterAnimationController.cs
+++ br06/Assets/Scripts/CharacterAnimationController.cs
@@ -9,10 +9,10 @@ public class CharacterAnimationController : MonoBehaviour
 
     // Speed variables
     public int PlayerNumber = 1;
-    public float WalkSpeed = 7.5f;
-    public float RunSpeed = 17.5f;
-    public float TurnSpeed = 200.0f;
-    public float StrafeSpeed = 5.0f;
+    //public float WalkSpeed = 7.5f;
+    public float RunSpeed = 12f;
+    //public float TurnSpeed = 200.0f;
+    //public float StrafeSpeed = 5.0f;
     public float Gravity = 1000.0f;
 
     void Start()
@@ -28,10 +28,10 @@ public class CharacterAnimationController : MonoBehaviour
         if (_characterController.isGrounded)
         {
             moveDirection = new Vector3(-Input.GetAxis("Vertical"+PlayerNumber), 0, Input.GetAxis("Horizontal"+PlayerNumber));
-            if (Input.GetButton("Run"+PlayerNumber))
+            //if (Input.GetButton("Run"+PlayerNumber))
                 moveDirection *= RunSpeed;
-            else
-                moveDirection *= WalkSpeed;
+            //else
+            //    moveDirection *= WalkSpeed;
             // DEVNOTE: Uncomment the following if jumping is added to the game
             /*if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;*/
@@ -47,15 +47,15 @@ public class CharacterAnimationController : MonoBehaviour
         // DEVNOTE: Rework Animator component due to movement system rework
         if (moveDirection.x != 0 || moveDirection.z != 0)
         {
-            _animator.SetInteger("walking", 1);
-            if (Input.GetButton("Run" + PlayerNumber))
+            //_animator.SetInteger("walking", 1);
+            //if (Input.GetButton("Run" + PlayerNumber))
                 _animator.SetBool("running", true);
-            else
-                _animator.SetBool("running", false);
+            //else
+            //    _animator.SetBool("running", false);
         }
         else
         {
-            _animator.SetInteger("walking", 0);
+            //_animator.SetInteger("walking", 0);
             _animator.SetBool("running", false);
         }
     }
