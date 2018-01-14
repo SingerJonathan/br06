d25cdf5e8d2723147aeb6fdf46e330d58cc8a729
DESKTOP-Andrew <13132563@studentmail.ul.ie>
2018-01-14 19:11:13 +0000

    TG-37 ability UI and cooldown script plus scriptable objects for abilities implemented

---
+++ br06/Assets/ScriptableObjects/Longsword/LSAbility1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSAbility1 : Ability {
+
+    public int WeaponDamage = 3;
+    public float WeaponRange = 10f;
+
+    public override void Initialise(GameObject obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TriggerAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ br06/Assets/Scripts/AbilityCooldown.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AbilityCooldown : MonoBehaviour
+{
+
+    public string abilityButtonAxisName = "Fire1";
+    public Image darkMask;
+    public Text CooldownTextDisplay;
+
+    [SerializeField] private Ability ability;
+    [SerializeField] private GameObject weaponHolder;
+    private Image myButtonImage;
+    private AudioSource abilitySource;
+    private float CooldownDuration;
+    private float nextReadyTime;
+    private float CooldownTimeLeft;
+
+
+    void Start()
+    {
+        Initialize(ability, weaponHolder);
+    }
+
+    public void Initialize(Ability selectedAbility, GameObject weaponHolder)
+    {
+        ability = selectedAbility;
+        myButtonImage = GetComponent<Image>();
+        abilitySource = GetComponent<AudioSource>();
+        myButtonImage.sprite = ability.aSprite;
+        darkMask.sprite = ability.aSprite;
+        CooldownDuration = ability.aBaseCooldown;
+        ability.Initialise(weaponHolder);
+        AbilityReady();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool CooldownComplete = (Time.time > nextReadyTime);
+        if (CooldownComplete)
+        {
+            AbilityReady();
+            if (Input.GetButtonDown(abilityButtonAxisName))
+            {
+                ButtonTriggered();
+            }
+        }
+        else
+        {
+            Cooldown();
+        }
+    }
+
+    private void AbilityReady()
+    {
+        CooldownTextDisplay.enabled = false;
+        darkMask.enabled = false;
+    }
+
+    private void Cooldown()
+    {
+        CooldownTimeLeft -= Time.deltaTime;
+        float roundedCd = Mathf.Round(CooldownTimeLeft);
+        CooldownTextDisplay.text = roundedCd.ToString();
+        darkMask.fillAmount = (CooldownTimeLeft / CooldownDuration);
+    }
+
+    private void ButtonTriggered()
+    {
+        nextReadyTime = CooldownDuration + Time.time;
+        CooldownTimeLeft = CooldownDuration;
+        darkMask.enabled = true;
+        CooldownTextDisplay.enabled = true;
+
+        abilitySource.clip = ability.aSound;
+        abilitySource.Play();
+        ability.TriggerAbility();
+    }
+}
