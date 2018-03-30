2c87655c7fd5095003dafd2c81542fb5674851a1
DESKTOP-Andrew <13132563@studentmail.ul.ie>
2018-03-30 19:22:54 +0100

    TG-93: First longsword mutation finished

---
+++ br06/Assets/Scripts/Abilities/Longsword/LongswordBleedMutation1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongswordBleedMutation1 : Ability
+{
+
+    public int WeaponDamage = 1;
+    public int VulnerableDamage = 1;
+    public float VulnerableCooldown = 1f;
+    private HitboxTriggerController _hitboxTriggerController;
+
+    public override void Initialize(GameObject obj)
+    {
+        _hitboxTriggerController = obj.transform.Find("HitboxSemiCircle").GetComponent<HitboxTriggerController>();
+        _enemyColour = obj.name.Contains("Red") ? "Blue" : "Red";
+    }
+
+    public override void TriggerAbility()
+    {
+        for (int index = 0; index < _hitboxTriggerController.CollidingObjects.Count; index++)
+        {
+            if (_hitboxTriggerController.CollidingObjects[index].name.Contains(_enemyColour))
+            {
+                Transform origin = _hitboxTriggerController.transform.parent.gameObject.transform.Find(MeleeAbility.headString);
+                Transform target = GameObject.FindGameObjectWithTag(_enemyColour).transform.Find(MeleeAbility.headString);
+                RaycastHit hit;
+                if (Physics.Raycast(origin.position, target.position - origin.position, out hit, 100, ~(1 << 8)))
+                {
+                    Debug.DrawRay(origin.position, target.position - origin.position, Color.red, 10);
+                    if (hit.transform.name.Contains(_enemyColour))
+                    {
+                        _hitboxTriggerController.CollidingObjects[index].GetComponent<CharacterStatsController>().DoDamage(WeaponDamage);
+                        _hitboxTriggerController.CollidingObjects[index].GetComponent<CharacterStatsController>().VulnerableAttack(VulnerableCooldown, VulnerableDamage);
+                    }
+                }
+            }
+        }
+    }
+
+}
